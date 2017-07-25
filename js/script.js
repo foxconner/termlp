a235b839f11c0977974841eafa2ea430d99aa8b5
foxconner <resys@ya.ru>
2017-07-25 16:41:48 +0600

    added php script for popup

--- js/script.js
+++ js/script.js
@@ -13,3 +13,19 @@ $(window).scroll(function(){
 	$('.popup').css({'top': $(window).scrollTop() + 
 		($(window).innerHeight() /2 - 168)});
 }).scroll();
+
+$("form").submit(function() { //Change
+	var th = $(this);
+	$.ajax({
+		type: "POST",
+		url: "mail.php", //Change
+		data: th.serialize()
+	}).done(function() {
+		alert("Thank you!");
+		setTimeout(function() {
+			// Done Functions
+			th.trigger("reset");
+		}, 1000);
+	});
+	return false;
+});
