25d303c579c6440cc8b8c33c2d770c7cdab19028
foxconner <resys@ya.ru>
2017-07-31 16:15:26 +0600

    some changes in html

--- js/script.js
+++ js/script.js
@@ -3,16 +3,18 @@ $('.open-popup').click(function(){
 		($(window).innerHeight() /2 - 168)}).addClass('active');
 	$('.bg-popup').fadeIn();
 
+	$(window).scroll(function(){
+		$('.popup').css({'top': $(window).scrollTop() + 
+			($(window).innerHeight() /2 - 168)});
+	}).scroll();
+
 	$('.bg-popup').click(function(){
 		$('.popup').removeClass('active');
 		$('.bg-popup').fadeOut();
 	});
 });
 
-$(window).scroll(function(){
-	$('.popup').css({'top': $(window).scrollTop() + 
-		($(window).innerHeight() /2 - 168)});
-}).scroll();
+
 
 $("form").submit(function() { //Change
 	var th = $(this);
